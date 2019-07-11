baa74b6665ca379b7cfc22b4d4ef5ed26b45d195
jack <lijiangfeng@ffcode.net>
2019-07-11 13:17:13 +0800

    显示调整

--- src/views/problem/problem.vue
+++ src/views/problem/problem.vue
@@ -13,9 +13,17 @@
       <Split v-model="split" min="500px" max="600px">
         <div
           slot="left"
-          style="overflow-y: auto; overflow-x: hidden;padding: 10px 20px;height: 100%"
-          v-html="problem"
-        ></div>
+          class="p-problem__desc"
+          style="overflow-y: auto; overflow-x: hidden;padding: 0 15px;height: 100%"
+        >
+          <h3>{{problem.title}}</h3>
+          <Divider orientation="left">描述</Divider>
+          <div class="desc" v-html="problem.description"></div>
+          <Divider orientation="left">输入</Divider>
+          <div class="desc" v-html="problem.inputDescription"></div>
+          <Divider orientation="left">输出</Divider>
+          <div class="desc" v-html="problem.outputDescription"></div>
+        </div>
         <Split slot="right" mode="vertical" v-model="vSplit">
           <f-panel slot="top" :header="false" style="height: 100%">
             <div slot="body" slot-scope="param">
@@ -70,7 +78,7 @@ export default {
       ids: [],
       split: 0.4,
       vSplit: 0.7,
-      problem: null,
+      problem: {},
       repoId: null,
       assignmentId: null,
       Terminal: null,
@@ -214,8 +222,8 @@ export default {
             })
             .then(rep => {
               this.getSubmitState(rep.submitId);
-              this.$Message.success("提交成功,请等待判题结果。");
-            });
+              this.Message.success("提交成功,请等待判题结果。");
+            });r
         }
       });
     },
@@ -281,6 +289,281 @@ export default {
       align-items: center;
       margin-left: 20px;
     }
+  }
+
+  &__desc {
+    h1, h2, h3, h4 {
+      color: #111111;
+      font-weight: 400;
+      margin-top: 1em;
+    }
+
+    h1, h2, h3, h4, h5 {
+      font-family: Georgia, Palatino, serif;
+    }
+    h1, h2, h3, h4, h5, p , dl{
+      margin-bottom: 16px;
+      padding: 0;
+    }
+    h1 {
+      font-size: 48px;
+      line-height: 54px;
+    }
+    h2 {
+      font-size: 36px;
+      line-height: 42px;
+    }
+    h1, h2 {
+      border-bottom: 1px solid #EFEAEA;
+      padding-bottom: 10px;
+    }
+    h3 {
+      font-size: 24px;
+      line-height: 30px;
+    }
+    h4 {
+      font-size: 21px;
+      line-height: 26px;
+    }
+    h5 {
+      font-size: 18px;
+      list-style: 23px;
+    }
+    a {
+      color: #0099ff;
+      margin: 0;
+      padding: 0;
+      vertical-align: baseline;
+    }
+    a:hover {
+      text-decoration: none;
+      color: #ff6600;
+    }
+    a:visited {
+      /*color: purple;*/
+    }
+    ul, ol {
+      padding: 0;
+      padding-left: 24px;
+      margin: 0;
+    }
+    li {
+      line-height: 24px;
+    }
+    p, ul, ol {
+      font-size: 16px;
+      line-height: 24px;
+    }
+
+    ol ol, ul ol {
+      list-style-type: lower-roman;
+    }
+
+    /*pre {
+        padding: 0px 24px;
+        max-width: 800px;
+        white-space: pre-wrap;
+    }
+    code {
+        font-family: Consolas, Monaco, Andale Mono, monospace;
+        line-height: 1.5;
+        font-size: 13px;
+    }*/
+
+    code, pre {
+      border-radius: 3px;
+      background-color:#f7f7f7;
+      color: inherit;
+    }
+
+    code {
+      font-family: Consolas, Monaco, Andale Mono, monospace;
+      margin: 0 2px;
+    }
+
+    pre {
+      line-height: 1.7em;
+      overflow: auto;
+      padding: 6px 10px;
+      border-left: 5px solid #6CE26C;
+    }
+
+    pre > code {
+      border: 0;
+      display: inline;
+      max-width: initial;
+      padding: 0;
+      margin: 0;
+      overflow: initial;
+      line-height: inherit;
+      font-size: .85em;
+      white-space: pre;
+      background: 0 0;
+
+    }
+
+    code {
+      color: #666555;
+    }
+
+
+    /** markdown preview plus 对于代码块的处理有些问题, 所以使用统一的颜色 */
+    /*code .keyword {
+      color: #8959a8;
+    }
+
+    code .number {
+      color: #f5871f;
+    }
+
+    code .comment {
+      color: #998
+    }*/
+
+    aside {
+      display: block;
+      float: right;
+      width: 390px;
+    }
+    blockquote {
+      border-left:.5em solid #eee;
+      padding: 0 0 0 2em;
+      margin-left:0;
+    }
+    blockquote  cite {
+      font-size:14px;
+      line-height:20px;
+      color:#bfbfbf;
+    }
+    blockquote cite:before {
+      content: '\2014 \00A0';
+    }
+
+    blockquote p {
+      color: #666;
+    }
+    hr {
+      text-align: left;
+      color: #999;
+      height: 2px;
+      padding: 0;
+      margin: 16px 0;
+      background-color: #e7e7e7;
+      border: 0 none;
+    }
+
+    dl {
+      padding: 0;
+    }
+
+    dl dt {
+      padding: 10px 0;
+      margin-top: 16px;
+      font-size: 1em;
+      font-style: italic;
+      font-weight: bold;
+    }
+
+    dl dd {
+      padding: 0 16px;
+      margin-bottom: 16px;
+    }
+
+    dd {
+      margin-left: 0;
+    }
+
+    /* Code below this line is copyright Twitter Inc. */
+
+    button,
+    input,
+    select,
+    textarea {
+      font-size: 100%;
+      margin: 0;
+      vertical-align: baseline;
+      *vertical-align: middle;
+    }
+    button, input {
+      line-height: normal;
+      *overflow: visible;
+    }
+    button::-moz-focus-inner, input::-moz-focus-inner {
+      border: 0;
+      padding: 0;
+    }
+    button,
+    input[type="button"],
+    input[type="reset"],
+    input[type="submit"] {
+      cursor: pointer;
+      -webkit-appearance: button;
+    }
+    input[type=checkbox], input[type=radio] {
+      cursor: pointer;
+    }
+    /* override default chrome & firefox settings */
+    input:not([type="image"]), textarea {
+      -webkit-box-sizing: content-box;
+      -moz-box-sizing: content-box;
+      box-sizing: content-box;
+    }
+
+    input[type="search"] {
+      -webkit-appearance: textfield;
+      -webkit-box-sizing: content-box;
+      -moz-box-sizing: content-box;
+      box-sizing: content-box;
+    }
+    input[type="search"]::-webkit-search-decoration {
+      -webkit-appearance: none;
+    }
+    label,
+    input,
+    select,
+    textarea {
+      font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+      font-size: 13px;
+      font-weight: normal;
+      line-height: normal;
+      margin-bottom: 18px;
+    }
+    input[type=checkbox], input[type=radio] {
+      cursor: pointer;
+      margin-bottom: 0;
+    }
+    input[type=text],
+    input[type=password],
+    textarea,
+    select {
+      display: inline-block;
+      width: 210px;
+      padding: 4px;
+      font-size: 13px;
+      font-weight: normal;
+      line-height: 18px;
+      height: 18px;
+      color: #808080;
+      border: 1px solid #ccc;
+      -webkit-border-radius: 3px;
+      -moz-border-radius: 3px;
+      border-radius: 3px;
+    }
+    select, input[type=file] {
+      height: 27px;
+      line-height: 27px;
+    }
+    textarea {
+      height: auto;
+    }
+    /* grey out placeholders */
+    :-moz-placeholder {
+      color: #bfbfbf;
+    }
+    ::-webkit-input-placeholder {
+      color: #bfbfbf;
+    }
+
   }
 
   #terminal {
